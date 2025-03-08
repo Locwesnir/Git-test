04ad3bce7b3c39a728b5a7389df79801c948a172
abdi <abdiprasetyo1005@gmail.com>
2025-03-08 09:34:53 +0800

    menambah paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>hello wordl!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum repudiandae autem aliquid et, dolore vero nulla dolor consequatur ratione? Molestiae magni excepturi aliquid repellendus aperiam suscipit velit dolor provident accusantium.</p>
 </body>
 </html>
